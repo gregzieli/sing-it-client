d1db97406fc0cb3851ffd6b819126e31c3ddd034
Grzegorz Zieliński <gregzieli@gmail.com>
2020-03-23 18:05:50 +0100

    Fetching from server

--- src/components/song-filter/song-filter.js
+++ src/components/song-filter/song-filter.js
@@ -1,22 +1,21 @@
-import React from 'react';
-
+import React from "react";
 
 class SongFilter extends React.Component {
-    handleChange(event) {
-      this.props.updateSearch(event.target.value);
-    }
-  
-    render() {
-      return (
-        <input
-          type="text"
-          placeholder="Search a song..."
-          className="input-search"
-          onChange={this.handleChange.bind(this)}
-          value={this.props.searchText}
-        />
-      );
-    }
+  handleChange(event) {
+    this.props.updateSearch(event.target.value);
   }
 
-  export default SongFilter;
+  render() {
+    return (
+      <input
+        type="text"
+        placeholder="Search a song..."
+        className="input-search"
+        onChange={this.handleChange.bind(this)}
+        value={this.props.searchText}
+      />
+    );
+  }
+}
+
+export default SongFilter;
