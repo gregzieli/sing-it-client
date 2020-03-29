85d5d0ce420668666880ccf42e60e4f14da37d2c
Grzegorz Zieliński <gregzieli@gmail.com>
2020-03-29 12:37:39 +0200

    song-stash: init

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.scss";
 import SongFilter from "../song-filter/song-filter";
 import SongList from "../song-list/song-list";
+import Stash from "../stash/stash";
 
 class App extends React.Component {
   constructor() {
@@ -16,7 +18,7 @@ class App extends React.Component {
   async componentDidMount() {
     const response = await fetch(`${process.env.REACT_APP_SERVER}/songs`);
     const songs = await response.json();
-    this.setState({ songs: songs })
+    this.setState({ songs: songs });
   }
 
   updateSearch(inputValue) {
@@ -27,14 +29,25 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="app">
-        <h1 className="app__title">Sing It!</h1>
-        <SongFilter
-          updateSearch={this.updateSearch.bind(this)}
-          searchText={this.state.filter}
-        />
-        <SongList filter={this.state.filter} songs={this.state.songs} />
-      </div>
+      <Router>
+        <div className="app">
+          <Link to="/">Songs</Link>
+          <Link to="/stash">Stash</Link>
+          <h1 className="app__title">Sing It!</h1>
+          <Switch>
+            <Route exact path="/">
+              <SongFilter
+                updateSearch={this.updateSearch.bind(this)}
+                searchText={this.state.filter}
+              />
+              <SongList filter={this.state.filter} songs={this.state.songs} />
+            </Route>
+            <Route path="/stash">
+              <Stash />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/stash/stash.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Stash extends Component {
+  render() {
+    return (
+      <h1>My stash</h1>
+    );
+  }
+}
+
+export default Stash;
