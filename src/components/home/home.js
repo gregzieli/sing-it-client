3c358bb68a1320ecdeb8000d97598beed06033f3
Grzegorz Zieliński <gregzieli@gmail.com>
2020-04-12 18:39:05 +0200

    added the loader

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import FilterableSongList from "../song-list/filterable-song-list";
+import Loader from "../../setup/loader";
 
 function Home({ songs }) {
   if (songs.length) {
     return <FilterableSongList songs={songs} />;
   }
 
-  return <div>Loading...</div>
+  return <Loader />
 }
 
 export default Home;
